d69106283c54228d3ea5f77836ad160b91c85696
Tim Lukens <tblukens87@gmail.com>
2018-09-08 15:39:36 -0500

    Change project app questions

--- project/quiz/app.js
+++ project/quiz/app.js
@@ -1,8 +1,5 @@
 var questions = [
-  new Question("Who was the first President of the United States?", [ "George Washington", "Thomas Jefferson", "Forrest Gump"], "George Washington"),
-  new Question("What is the best way to greet someone on the phone?", ["What?", "Yeah, buddy!", "Shut up."], "Yeah, buddy!"),
-  new Question("Who has intimate relations with squirrels?", ["Mitch", "Cody", "Everyone"], "Cody"),
-  new Question("On what day do we usually get donuts?", ["Mondey", "Sundey", "Wednesdey"], "Wednesdey")
+  new Question("Who was the first President of the United States?", [ "George Washington", "Thomas Jefferson", "Forrest Gump"], "George Washington")
 ];
 
 
