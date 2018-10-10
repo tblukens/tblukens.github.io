a6235be8d6a4b4a598cba7a7fc8d8174d37d29ed
Tim L <tblukens87@gmail.com>
2018-10-09 19:58:45 -0500

    Making modifications to portfolio information

--- js/me.js
+++ js/me.js
@@ -1,48 +1,10 @@
-// $('.jumbotron').hide();
-
-// $('.jumbotron')
-
-//   .delay(500).animate({ height: 'toggle', opacity: '0.4'}, "slow")
-
-//   .delay(500).animate({opacity: '1'}, "fast");
-
-
-
-// $("#projectContainer:nth-child(1n+1)").each(function() {
-
-//   $(this).delay(1000).animate({
-
-//      height: 'toggle'}, 1000, 'linear'
-
-//   );
-
-// });
-
-
-{/* <li class="list-group-item">HTML</li>
-<li class="list-group-item">CSS</li>
-<li class="list-group-item">Bootstrap</li>
-<li class="list-group-item">JavaScript</li>
-</ul>
-</div>
-</div>
-<div class="col-lg order-lg-3">
-<h3 class="mb-4">Learning Languages:</h3>
-<div class="list-group" id="learningLanguages">
-<ul class="list-group">
-<li class="list-group-item">JavaScript, always more to learn.</li>
-<li class="list-group-item">jQuery</li>
-<li class="list-group-item">Node.js</li>
-<li class="list-group-item">SQL</li>
-<li class="list-group-item">MongoDB</li>
-<li class="list-group-item">React</li> */}
-
 var currentLanguages = [
   'HTML',
   'CSS',
   'Bootstrap',
   'JavaScript',
   'jQuery',
+  'Firebase'
 ];
 
 var learningLanguages = [
@@ -56,25 +18,114 @@ var learningLanguages = [
 
 
 
-$(document).ready(function(){
+$(document).ready(function () {
 
-for (let i = 0; i < currentLanguages.length; i++) {
-  var addLi = $('<li>');
-  addLi.addClass('list-group-item');
-  addLi.text(currentLanguages[i]);
-  $('#currentLanguages').append(addLi);
-}
-for (let i = 0; i < learningLanguages.length; i++) {
-  var addLi = $('<li>');
-  addLi.addClass('list-group-item');
-  addLi.text(learningLanguages[i]);
-  $('#learningLanguages').append(addLi);
-}
+  for (let i = 0; i < currentLanguages.length; i++) {
+    var addLi = $('<li>');
+    addLi.addClass('list-group-item');
+    addLi.text(currentLanguages[i]);
+    $('#currentLanguages').append(addLi);
+  }
+  for (let i = 0; i < learningLanguages.length; i++) {
+    var addLi = $('<li>');
+    addLi.addClass('list-group-item');
+    addLi.text(learningLanguages[i]);
+    $('#learningLanguages').append(addLi);
+  }
 
 
-$(".list-group-item:even").addClass("list-group-item-custom");
+  $(".list-group-item:even").addClass("list-group-item-custom");
 
-$(".list-group-item:odd").addClass("list-group-item-secondary");
+  $(".list-group-item:odd").addClass("list-group-item-secondary");
+
+  var gitHubLink = "https://tblukens.github.io/";
+
+  var projects = [{
+    name: "Word Guess Game",
+    gitHub: "Word-Guess-Game",
+    imgLink: "../img/word_guess_game_portfolio.PNG",
+    
+  }]
+
+  var displayProjects
+
+  {
+    /* <div id="project1" class="col-md-6 col-lg-4">
+    <div class="card mb-3 border border-dark">
+      <a href="https://tblukens.github.io/Word-Guess-Game/" target="_blank">
+        <img class="card-img-top project-image" src="img/word_guess_game_portfolio.PNG" alt="Word Guess Game" />
+      </a>
+      <div class="card-body p-2 border border-dark border-bottom-0 border-left-0 border-right-0">
+        <h4 class="card-title">
+          <a href="https://tblukens.github.io/Word-Guess-Game/" target="_blank">Word Guess Game</a>
+        </h4>
+        <p class="card-text">Made with vanilla JavaScript</p>
+        <p class="small">Click above to try it out. Or below for github repo:</p>
+        <p class="small text-center"><a name="wordGuessGameButton" id="wordGuessGameButton" class="btn btn-sm btn-secondary border border-dark" href="https://github.com/tblukens/Word-Guess-Game" role="button">Link to the GitHub</a></p>
+      </div>
+    </div>
+    </div>
+    <div id="project2" class="col-md-6 col-lg-4">
+    <div class="card mb-3 border border-dark">
+      <a href="https://tblukens.github.io/unit-4-game/" target="_blank">
+        <img class="card-img-top project-image" src="img/dbz_fighter.png" alt="DBZ Fighter RPG">
+      </a>
+      <div class="card-body p-2 border border-dark border-bottom-0 border-left-0 border-right-0">
+        <a href="https://tblukens.github.io/unit-4-game/" target="_blank">
+          <h4 class="card-title">DBZ Fighter RPG</h4>
+        </a>
+        <p class="card-text">Made with JavaScript and jQuery</p>
+        <p class="small">Click above to try it out. Or below for github repo:</p>
+        <p class="small text-center"><a name="dbzFighterButton" id="dbzFighterButton" class="btn btn-sm btn-secondary border border-dark" href="https://github.com/tblukens/unit-4-game" role="button">Link to the GitHub</a></p>
+      </div>
+    </div>
+    </div>
+    <div id="project3" class="col-md-6 col-lg-4">
+    <div class="card mb-3 border border-dark">
+      <a href="https://tblukens.github.io/TriviaGame/" target="_blank">
+        <img class="card-img-top project-image" src="img/GAME_OF_THRONES.png" alt="Game of Thrones Trivia">
+      </a>
+      <div class="card-body p-2 border border-dark border-bottom-0 border-left-0 border-right-0">
+        <a href="https://tblukens.github.io/TriviaGame/" target="_blank">
+          <h4 class="card-title">Game of Thrones Trivia</h4>
+        </a>
+        <p class="card-text">Made with JavaScript and jQuery</p>
+        <p class="small">Click above to try it out. Or below for github repo:</p>
+        <p class="small text-center"><a name="triviaGameButton" id="triviaGameButton" class="btn btn-secondary border border-dark btn-sm" href="https://github.com/tblukens/TriviaGame" role="button">Link to the GitHub</a></p>
+      </div>
+    </div>
+    </div>
+    <div id="project4" class="col-md-6 col-lg-4">
+    <div class="card mb-3 border border-dark">
+      <a href="https://tblukens.github.io/GIPHY_API/" target="_blank">
+        <img class="card-img-top project-image" src="img/giphy.png" alt="GIPHY API">
+      </a>
+      <div class="card-body p-2 border border-dark border-bottom-0 border-left-0 border-right-0">
+        <a href="https://tblukens.github.io/GIPHY_API/" target="_blank">
+          <h4 class="card-title">Giphy API and OMDB Info</h4>
+        </a>
+        <p class="card-text">Made with JavaScript, jQuery, AJAX, APIs</p>
+        <p class="small">Click above to try it out. Or below for github repo:</p>
+        <p class="small text-center"><a name="giphyButton" id="giphyButton" class="btn btn-secondary border border-dark btn-sm" href="https://github.com/tblukens/GIPHY_API" role="button">Link to the GitHub</a></p>
+      </div>
+    </div>
+    </div>
+    <div id="project5" class="col-md-6 col-lg-4">
+    <div class="card mb-3 border border-dark">
+      <a href="https://tblukens.github.io/RPS-Multiplayer/" target="_blank">
+        <img class="card-img-top project-image" src="img/rps-multi.png" alt="RPS-Multiplayer">
+      </a>
+      <div class="card-body p-2 border border-dark border-bottom-0 border-left-0 border-right-0">
+        <a href="https://tblukens.github.io/RPS-Multiplayer/" target="_blank">
+          <h4 class="card-title">Rock Paper Scissors</h4>
+        </a>
+        <p class="card-text">Multiplayer rock, paper, scissors game made with JavaScript, jQuery, Firebase</p>
+        <p class="small">Click above to try it out. Or below for github repo:</p>
+        <p class="small text-center"><a name="giphyButton" id="giphyButton" class="btn btn-secondary border border-dark btn-sm" href="https://github.com/tblukens/RPS-Multiplayer" role="button">Link to the GitHub</a></p>
+      </div>
+    </div>
+    </div> */
+  }
 
 
 
@@ -82,7 +133,7 @@ $(".list-group-item:odd").addClass("list-group-item-secondary");
 
 
 
-$("#about, .contact-social, .project-image").each(function() {
+$("#about, .contact-social, .project-image").each(function () {
 
   $(this).hide();
 
@@ -90,19 +141,18 @@ $("#about, .contact-social, .project-image").each(function() {
 
 
 
-$('#about, .contact-social, .project-image').each(function(i) {
+$('#about, .contact-social, .project-image').each(function (i) {
 
-   delay =(i)*550;
+  delay = (i) * 550;
 
-   $(this).delay(delay).animate({
+  $(this).delay(delay).animate({
 
-      height: 'toggle'
+    height: 'toggle'
 
-    }, {
+  }, {
 
     duration: 1500,
 
   });
 
 });
-
--- js/simplefade.js
+++ js/simplefade.js
@@ -2,10 +2,10 @@ $('.centered').hide();
 $('.centered').animate({
   width: 'toggle'}, 1000
 );
-$('#cog1,#coming-soon,#cog2').hide();
-$('#cog1,#coming-soon,#cog2').delay(1000).animate({
-   height: 'toggle'}, 1000, 'linear'
-);
+// $('#cog1,#coming-soon,#cog2').hide();
+// $('#cog1,#coming-soon,#cog2').delay(1000).animate({
+//    height: 'toggle'}, 1000, 'linear'
+// );
 
 
 // $('#cog1').hide().delay(2000).fadeIn(1000);
