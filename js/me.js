ab74b3afa67303a7052f2d4a30c8a0eb346df366
Tim Lukens <tblukens87@gmail.com>
2018-11-11 13:57:56 -0600

    Refactoring javascript

--- js/me.js
+++ js/me.js
@@ -2,131 +2,42 @@ var currentLanguages = [
   'HTML',
   'CSS',
   'Bootstrap',
+  'Bulma CSS',
   'JavaScript',
   'jQuery',
   'Firebase',
   'APIs & AJAX',
+  'Node.js',
+  'SQL',
+  'Vue'
 ];
 
 var learningLanguages = [
   'JavaScript, always more to learn',
-  'Node.js',
-  'SQL',
   'MongoDB',
   'React',
-  'Vue'
+  'Sequelize',
 ];
 
-
+const listGroup = (col, name) => {
+  for (let i = 0; i < col.length; i++) {
+    let element = col[i];
+    var addLi = $('<li>');
+    addLi.addClass('list-group-item');
+    if (i % 2 === 0) {
+      addLi.addClass("list-group-item-custom");
+    } else {
+      addLi.addClass("list-group-item-secondary");
+    }
+    addLi.text(element);
+    $(name).append(addLi);
+  }
+}
 
 $(document).ready(function () {
 
-  for (let i = 0; i < currentLanguages.length; i++) {
-    var addLi = $('<li>');
-    addLi.addClass('list-group-item');
-    addLi.text(currentLanguages[i]);
-    $('#currentLanguages').append(addLi);
-  }
-  for (let i = 0; i < learningLanguages.length; i++) {
-    var addLi = $('<li>');
-    addLi.addClass('list-group-item');
-    addLi.text(learningLanguages[i]);
-    $('#learningLanguages').append(addLi);
-  }
-
-
-  $(".list-group-item:even").addClass("list-group-item-custom");
-
-  $(".list-group-item:odd").addClass("list-group-item-secondary");
-
-  var projects = [
-    {
-      name: "Word Guess Game",
-      gitHub: "Word-Guess-Game",
-      imgLink: "./img/word_guess_game_portfolio.PNG",
-      madeWith: "Vanilla JavaScript",
-      webApp: true
-    },
-    {
-      name: "DBZ Fighter RPG",
-      gitHub: "unit-4-game",
-      imgLink: "./img/dbz_fighter.png",
-      madeWith: "JavaScript and jQuery",
-      webApp: true
-    },
-    {
-      name: "Game of Thrones Trivia",
-      gitHub: "TriviaGame",
-      imgLink: "./img/GAME_OF_THRONES.png",
-      madeWith: "JavaScript and jQuery",
-      webApp: true
-    },
-    {
-      name: "Giphy API and OMDB Info",
-      gitHub: "GIPHY_API",
-      imgLink: "./img/giphy.png",
-      madeWith: "JavaScript, jQuery, AJAX, APIs",
-      webApp: true
-    },
-    {
-      name: "Multiplayer Rock, Paper, Scissors",
-      gitHub: "RPS-Multiplayer",
-      imgLink: "./img/rps-multi.png",
-      madeWith: "JavaScript, jQuery, Firebase",
-      webApp: true
-    },
-    {
-      name: "What's For Dinner?",
-      gitHub: "Whats_For_Dinner",
-      imgLink: "./img/portfolio-whats-for-dinner.png",
-      madeWith: "JavaScript, jQuery, AJAX/APIs, html2canvas, Bulma CSS",
-      webApp: true
-    },
-    {
-      name: "LIRI Node App",
-      gitHub: "liri-node-app",
-      imgLink: "./img/liri-node-app.png",
-      madeWith: "JavaScript, Node.js, and node modules.",
-      webApp: false
-    },
-    {
-      name: "Word Guess Game - Node",
-      gitHub: "node_word_guess_game",
-      imgLink: "./img/node_word_guess_game.png",
-      madeWith: "JavaScript, Node.js, and node modules.",
-      webApp: false
-    },
-    {
-      name: "Bamazon Store - Node/MySQL",
-      gitHub: "bamazon_node_mysql",
-      imgLink: "./img/bamazon.png",
-      madeWith: "JavaScript, Node.js, MySQL",
-      webApp: false
-    },
-    {
-      name: "Node Bank App - Node/MySQL",
-      gitHub: "bank_of_node",
-      imgLink: "./img/bank_of_node.png",
-      madeWith: "JavaScript, Node.js, MySQL",
-      webApp: false
-    },
-    {
-      name: "Friend Finder",
-      gitHub: "Friend-Finder-Vue",
-      imgLink: "./img/friend-finder.png",
-      madeWith: "Vue, Node, Express",
-      webApp: 'heroku',
-      herokuLink: "https://tbl-friend-finder.herokuapp.com/"
-    },
-    {
-      name: "Eat Da Burger",
-      gitHub: "burger",
-      imgLink: "./img/eat_da_burger.png",
-      madeWith: "Vue, Node, Express, MySQL",
-      webApp: 'heroku',
-      herokuLink: "http://tbl-burger.herokuapp.com/"
-    }
-  ]
+  listGroup(currentLanguages, "#currentLanguages");
+  listGroup(learningLanguages, "#learningLanguages");
 
   projects.forEach(function (element, i) {
     console.log(element.herokuLink)
---
+++ js/projects.js
@@ -0,0 +1,88 @@
+var projects = [
+    {
+      name: "Word Guess Game",
+      gitHub: "Word-Guess-Game",
+      imgLink: "./img/word_guess_game_portfolio.PNG",
+      madeWith: "Vanilla JavaScript",
+      webApp: true
+    },
+    {
+      name: "DBZ Fighter RPG",
+      gitHub: "unit-4-game",
+      imgLink: "./img/dbz_fighter.png",
+      madeWith: "JavaScript and jQuery",
+      webApp: true
+    },
+    {
+      name: "Game of Thrones Trivia",
+      gitHub: "TriviaGame",
+      imgLink: "./img/GAME_OF_THRONES.png",
+      madeWith: "JavaScript and jQuery",
+      webApp: true
+    },
+    {
+      name: "Giphy API and OMDB Info",
+      gitHub: "GIPHY_API",
+      imgLink: "./img/giphy.png",
+      madeWith: "JavaScript, jQuery, AJAX, APIs",
+      webApp: true
+    },
+    {
+      name: "Multiplayer Rock, Paper, Scissors",
+      gitHub: "RPS-Multiplayer",
+      imgLink: "./img/rps-multi.png",
+      madeWith: "JavaScript, jQuery, Firebase",
+      webApp: true
+    },
+    {
+      name: "What's For Dinner?",
+      gitHub: "Whats_For_Dinner",
+      imgLink: "./img/portfolio-whats-for-dinner.png",
+      madeWith: "JavaScript, jQuery, AJAX/APIs, html2canvas, Bulma CSS",
+      webApp: true
+    },
+    {
+      name: "LIRI Node App",
+      gitHub: "liri-node-app",
+      imgLink: "./img/liri-node-app.png",
+      madeWith: "JavaScript, Node.js, and node modules.",
+      webApp: false
+    },
+    {
+      name: "Word Guess Game - Node",
+      gitHub: "node_word_guess_game",
+      imgLink: "./img/node_word_guess_game.png",
+      madeWith: "JavaScript, Node.js, and node modules.",
+      webApp: false
+    },
+    {
+      name: "Bamazon Store - Node/MySQL",
+      gitHub: "bamazon_node_mysql",
+      imgLink: "./img/bamazon.png",
+      madeWith: "JavaScript, Node.js, MySQL",
+      webApp: false
+    },
+    {
+      name: "Node Bank App - Node/MySQL",
+      gitHub: "bank_of_node",
+      imgLink: "./img/bank_of_node.png",
+      madeWith: "JavaScript, Node.js, MySQL",
+      webApp: false
+    },
+    {
+      name: "Friend Finder",
+      gitHub: "Friend-Finder-Vue",
+      imgLink: "./img/friend-finder.png",
+      madeWith: "Vue, Node, Express",
+      webApp: 'heroku',
+      herokuLink: "https://tbl-friend-finder.herokuapp.com/"
+    },
+    {
+      name: "Eat Da Burger",
+      gitHub: "burger",
+      imgLink: "./img/eat_da_burger.png",
+      madeWith: "Vue, Node, Express, MySQL",
+      webApp: 'heroku',
+      herokuLink: "http://tbl-burger.herokuapp.com/"
+    }
+  ]
