12baec0d9ae8f1465b05ecbc3128eb5038a68616
Tim Lukens <tblukens87@gmail.com>
2018-11-11 14:00:49 -0600

    More refactoring. Removed bootstrap from js folder

--- js/me.js
+++ js/me.js
@@ -1,23 +1,4 @@
-var currentLanguages = [
-  'HTML',
-  'CSS',
-  'Bootstrap',
-  'Bulma CSS',
-  'JavaScript',
-  'jQuery',
-  'Firebase',
-  'APIs & AJAX',
-  'Node.js',
-  'SQL',
-  'Vue'
-];
 
-var learningLanguages = [
-  'JavaScript, always more to learn',
-  'MongoDB',
-  'React',
-  'Sequelize',
-];
 
 const listGroup = (col, name) => {
   for (let i = 0; i < col.length; i++) {
---
+++ js/skills.js
@@ -0,0 +1,20 @@
+var currentLanguages = [
+    'HTML',
+    'CSS',
+    'Bootstrap',
+    'Bulma CSS',
+    'JavaScript',
+    'jQuery',
+    'Firebase',
+    'APIs & AJAX',
+    'Node.js',
+    'SQL',
+    'Vue'
+  ];
+  
+  var learningLanguages = [
+    'JavaScript, always more to learn',
+    'MongoDB',
+    'React',
+    'Sequelize',
+  ];
