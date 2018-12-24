dfc500dc513c87cf76e3f5d7e0c41afed4b060a4
Tim Lukens <tblukens87@gmail.com>
2018-12-24 17:48:33 -0600

    Changed about me section and skills

--- js/skills.js
+++ js/skills.js
@@ -9,12 +9,14 @@ var currentLanguages = [
     'APIs & AJAX',
     'Node.js',
     'SQL',
+    'MongoDB',
+    'React',
+    'Sequelize',
     'Vue'
   ];
   
   var learningLanguages = [
     'JavaScript, always more to learn',
-    'MongoDB',
-    'React',
-    'Sequelize',
+    'PHP',
+    'Java'
   ];
