7610db20614ce23b5f101a961f32a35b1229a82b
Tim Lukens <tblukens87@gmail.com>
2019-01-06 17:31:35 -0600

    Added mongo scraper homework

--- js/projects.js
+++ js/projects.js
@@ -1,109 +1,130 @@
 var projects = [
-    {
-      name: "Word Guess Game",
-      gitHub: "Word-Guess-Game",
-      imgLink: "./img/word_guess_game_portfolio.PNG",
-      madeWith: "Vanilla JavaScript",
-      shortDescription: "A word guess game made with Vanilla JavaScript",
-      webApp: true
-    },
-    {
-      name: "DBZ Fighter RPG",
-      gitHub: "unit-4-game",
-      imgLink: "./img/dbz_fighter.png",
-      madeWith: "JavaScript and jQuery",
-      shortDescription: "Fun fighter game with DBZ characters made with jQuery",
-      webApp: true
-    },
-    {
-      name: "Game of Thrones Trivia",
-      gitHub: "TriviaGame",
-      imgLink: "./img/GAME_OF_THRONES.png",
-      madeWith: "JavaScript and jQuery",
-      shortDescription: "A Game of Thrones Trivia Game made with jQuery and Bootstrap",
-      webApp: true
-    },
-    {
-      name: "Giphy API and OMDB Info",
-      gitHub: "GIPHY_API",
-      imgLink: "./img/giphy.png",
-      madeWith: "JavaScript, jQuery, AJAX, APIs",
-      shortDescription: "Fun gif search using Giphy API to request gifs. AJAX to get Giphy API info and jQuery to display.",
-      webApp: true
-    },
-    {
-      name: "Multiplayer Rock, Paper, Scissors",
-      gitHub: "RPS-Multiplayer",
-      imgLink: "./img/rps-multi.png",
-      madeWith: "JavaScript, jQuery, Firebase",
-      shortDescription: "Rock Paper Scissors game made with jQuery and uses Firebase as a database.",
-      webApp: true
-    },
-    {
-      name: "What's For Dinner?",
-      gitHub: "Whats_For_Dinner",
-      imgLink: "./img/portfolio-whats-for-dinner.png",
-      madeWith: "JavaScript, jQuery, AJAX/APIs, html2canvas, Bulma CSS",
-      shortDescription: "What's for dinner. Group project where I worked creating the API calls and jQuery interaction. A neat application that allows users to search recipes and restaurants.",
-      webApp: true
-    },
-    {
-      name: "LIRI Node App",
-      gitHub: "liri-node-app",
-      imgLink: "./img/liri-node-app.png",
-      madeWith: "JavaScript, Node.js, and node modules.",
-      shortDescription: "Simple command line application using APIs to search based on commands.",
-      webApp: false
-    },
-    {
-      name: "Word Guess Game - Node",
-      gitHub: "node_word_guess_game",
-      imgLink: "./img/node_word_guess_game.png",
-      madeWith: "JavaScript, Node.js, and node modules.",
-      shortDescription: "A command line application where a user inputs a letter to guess a word.",
-      webApp: false
-    },
-    {
-      name: "Bamazon Store - Node/MySQL",
-      gitHub: "bamazon_node_mysql",
-      imgLink: "./img/bamazon.png",
-      madeWith: "JavaScript, Node.js, MySQL",
-      shortDescription: "Command line application using MySQL to store item information and more!",
-      webApp: false
-    },
-    {
-      name: "Node Bank App - Node/MySQL",
-      gitHub: "bank_of_node",
-      imgLink: "./img/bank_of_node.png",
-      madeWith: "JavaScript, Node.js, MySQL",
-      shortDescription: "Command line application with simple login using MySQL to store user information.",
-      webApp: false
-    },
-    {
-      name: "Friend Finder",
-      gitHub: "Friend-Finder-Vue",
-      imgLink: "./img/friend-finder.png",
-      madeWith: "Vue, Node, Express",
-      shortDescription: "Friend Finder application using Node.js and Express for back end. Using Vue.js as a front end.",
-      webApp: 'heroku',
-      herokuLink: "https://tbl-friend-finder.herokuapp.com/"
-    },
-    {
-      name: "Eat Da Burger",
-      gitHub: "burger",
-      imgLink: "./img/eat_da_burger.png",
-      madeWith: "Vue, Node, Express, MySQL",
-      shortDescription: "An application where a user can create a burger to then consume once created. Burgers are stored in MySQL. Front end was made with VueJS. Express made with Node and ExpressJS",
-      webApp: 'heroku',
-      herokuLink: "http://tbl-burger.herokuapp.com/"
-    },
-    {
-      name: "Christmas Wish List",
-      gitHub: "https://github.com/keenwilson/ChristmasList",
-      imgLink: "./img/portfolio-christmaslist.png",
-      madeWith: "Vue, Node, Express, MySQL",
-      shortDescription: "A fun application where users can search Walmart API for items to add to a Wish List. App uses MySQL to store User information and saved wish list item info.",
-      webApp: 'heroku',
-      herokuLink: "https://tbl-christmas-wishlist.herokuapp.com/"
-    }
-  ]
+  {
+    name: 'Word Guess Game',
+    gitHub: 'Word-Guess-Game',
+    imgLink: './img/word_guess_game_portfolio.PNG',
+    madeWith: 'Vanilla JavaScript',
+    shortDescription: 'A word guess game made with Vanilla JavaScript',
+    webApp: true
+  },
+  {
+    name: 'DBZ Fighter RPG',
+    gitHub: 'unit-4-game',
+    imgLink: './img/dbz_fighter.png',
+    madeWith: 'JavaScript and jQuery',
+    shortDescription: 'Fun fighter game with DBZ characters made with jQuery',
+    webApp: true
+  },
+  {
+    name: 'Game of Thrones Trivia',
+    gitHub: 'TriviaGame',
+    imgLink: './img/GAME_OF_THRONES.png',
+    madeWith: 'JavaScript and jQuery',
+    shortDescription:
+      'A Game of Thrones Trivia Game made with jQuery and Bootstrap',
+    webApp: true
+  },
+  {
+    name: 'Giphy API and OMDB Info',
+    gitHub: 'GIPHY_API',
+    imgLink: './img/giphy.png',
+    madeWith: 'JavaScript, jQuery, AJAX, APIs',
+    shortDescription:
+      'Fun gif search using Giphy API to request gifs. AJAX to get Giphy API info and jQuery to display.',
+    webApp: true
+  },
+  {
+    name: 'Multiplayer Rock, Paper, Scissors',
+    gitHub: 'RPS-Multiplayer',
+    imgLink: './img/rps-multi.png',
+    madeWith: 'JavaScript, jQuery, Firebase',
+    shortDescription:
+      'Rock Paper Scissors game made with jQuery and uses Firebase as a database.',
+    webApp: true
+  },
+  {
+    name: "What's For Dinner?",
+    gitHub: 'Whats_For_Dinner',
+    imgLink: './img/portfolio-whats-for-dinner.png',
+    madeWith: 'JavaScript, jQuery, AJAX/APIs, html2canvas, Bulma CSS',
+    shortDescription:
+      "What's for dinner. Group project where I worked creating the API calls and jQuery interaction. A neat application that allows users to search recipes and restaurants.",
+    webApp: true
+  },
+  {
+    name: 'LIRI Node App',
+    gitHub: 'liri-node-app',
+    imgLink: './img/liri-node-app.png',
+    madeWith: 'JavaScript, Node.js, and node modules.',
+    shortDescription:
+      'Simple command line application using APIs to search based on commands.',
+    webApp: false
+  },
+  {
+    name: 'Word Guess Game - Node',
+    gitHub: 'node_word_guess_game',
+    imgLink: './img/node_word_guess_game.png',
+    madeWith: 'JavaScript, Node.js, and node modules.',
+    shortDescription:
+      'A command line application where a user inputs a letter to guess a word.',
+    webApp: false
+  },
+  {
+    name: 'Bamazon Store - Node/MySQL',
+    gitHub: 'bamazon_node_mysql',
+    imgLink: './img/bamazon.png',
+    madeWith: 'JavaScript, Node.js, MySQL',
+    shortDescription:
+      'Command line application using MySQL to store item information and more!',
+    webApp: false
+  },
+  {
+    name: 'Node Bank App - Node/MySQL',
+    gitHub: 'bank_of_node',
+    imgLink: './img/bank_of_node.png',
+    madeWith: 'JavaScript, Node.js, MySQL',
+    shortDescription:
+      'Command line application with simple login using MySQL to store user information.',
+    webApp: false
+  },
+  {
+    name: 'Friend Finder',
+    gitHub: 'Friend-Finder-Vue',
+    imgLink: './img/friend-finder.png',
+    madeWith: 'Vue, Node, Express',
+    shortDescription:
+      'Friend Finder application using Node.js and Express for back end. Using Vue.js as a front end.',
+    webApp: 'heroku',
+    herokuLink: 'https://tbl-friend-finder.herokuapp.com/'
+  },
+  {
+    name: 'Eat Da Burger',
+    gitHub: 'burger',
+    imgLink: './img/eat_da_burger.png',
+    madeWith: 'Vue, Node, Express, MySQL',
+    shortDescription:
+      'An application where a user can create a burger to then consume once created. Burgers are stored in MySQL. Front end was made with VueJS. Express made with Node and ExpressJS',
+    webApp: 'heroku',
+    herokuLink: 'http://tbl-burger.herokuapp.com/'
+  },
+  {
+    name: 'Christmas Wish List',
+    gitHub: 'https://github.com/keenwilson/ChristmasList',
+    imgLink: './img/portfolio-christmaslist.png',
+    madeWith: 'Vue, Node, Express, MySQL',
+    shortDescription:
+      'A fun application where users can search Walmart API for items to add to a Wish List. App uses MySQL to store User information and saved wish list item info.',
+    webApp: 'heroku',
+    herokuLink: 'https://tbl-christmas-wishlist.herokuapp.com/'
+  },
+  {
+    name: 'Mongo Scraper',
+    gitHub: 'Cheerio_Mongo',
+    imgLink: './img/mongo-scraper.png',
+    madeWith: 'React, Node, Express, Axios, MongoDB, Mongoose',
+    shortDescription:
+      'App using Cheerio to scrape website data from PCGAMER news to save into a mongo database then allow users to set notes and view articles.',
+    webApp: 'heroku',
+    herokuLink: 'https://secure-thicket-49434.herokuapp.com/'
+  }
+];
--- js/skills.js
+++ js/skills.js
@@ -1,22 +1,14 @@
 var currentLanguages = [
-    'HTML',
-    'CSS',
-    'Bootstrap',
-    'Bulma CSS',
-    'JavaScript',
-    'jQuery',
-    'Firebase',
-    'APIs & AJAX',
-    'Node.js',
-    'SQL',
-    'MongoDB',
-    'React',
-    'Sequelize',
-    'Vue'
-  ];
-  
-  var learningLanguages = [
-    'JavaScript, always more to learn',
-    'PHP',
-    'Java'
-  ];
+  'HTML',
+  'CSS',
+  'JavaScript',
+  'jQuery',
+  'APIs',
+  'Node.js',
+  'SQL',
+  'MongoDB',
+  'React',
+  'Vue'
+];
+
+var learningLanguages = ['PHP', 'Java'];
